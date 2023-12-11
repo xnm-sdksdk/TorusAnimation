a7687a6fd8fb7673773ac38b327ebe6c26b37105
xnm-sdksdk <n.mendonca@protonmail.com>
2023-12-11 23:54:24 +0000

    smalls bugs fix

--- main.js
+++ main.js
@@ -1,7 +1,4 @@
-import "./style.css";
-
-import * as THREE from "three";
-import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
+import * as THREE from './node_modules/three/build/three.module.js';
 
 const scene = new THREE.Scene();
 
